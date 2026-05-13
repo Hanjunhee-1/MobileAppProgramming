3e20b2ee54e0193b15fb37183785bb06b70a4029
Hanjunhee-1 <hanjuni857@naver.com>
2026-05-14 01:12:16 +0900

    feat: TODO 완료 처리 시에 Gold 지급 로직 추가

--- Server/Services/todoService.js
+++ Server/Services/todoService.js
@@ -126,6 +126,8 @@ async function updateTodo(userId, todoId, body) {
 
     const todos = readJson(todoPath);
 
+    const users = readJson(userPath);
+
     const index = todos.findIndex(
         todo =>
             todo.id === Number(todoId) &&
@@ -136,11 +138,98 @@ async function updateTodo(userId, todoId, body) {
         throw new Error("Todo를 찾을 수 없습니다.");
     }
 
+    const wasCompleted =
+        todos[index].is_completed;
+
+    // 완료 취소 방지
+    if (
+        wasCompleted &&
+        body.is_completed === false
+    ) {
+        throw new Error(
+            "완료 상태는 취소할 수 없습니다."
+        );
+    }
+
     todos[index] = {
         ...todos[index],
         ...body,
         updated_at: new Date().toISOString()
     };
+
+    const willBeCompleted =
+        todos[index].is_completed;
+
+    if (
+        wasCompleted &&
+        willBeCompleted
+    ) {
+        throw new Error(
+            "이미 완료된 Todo입니다."
+        );
+    }
+
+    /**
+     * false -> true
+     * 완료 처리
+     */
+    if (
+        !wasCompleted &&
+        willBeCompleted
+    ) {
+
+        const currentValue =
+            calculateValue(
+                todos[index]
+            );
+
+        let rewardGold =
+            currentValue;
+
+        const now = new Date();
+
+        const endDate =
+            new Date(
+                todos[index].time.end
+            );
+
+        const remainingMs =
+            endDate - now;
+
+        const remainingDays =
+            remainingMs /
+            (1000 * 60 * 60 * 24);
+
+        // 마감 1일 이내 보너스
+        if (remainingDays <= 1) {
+
+            rewardGold =
+                Math.round(
+                    rewardGold * 1.2
+                );
+        }
+
+        todos[index].current_value =
+            calculateValue(todos[index]);
+
+        todos[index].reward_gold =
+            rewardGold;
+
+        todos[index].completed_at =
+            new Date().toISOString();
+
+        // 유저 Gold 지급
+        const userIndex =
+            users.findIndex(
+                user =>
+                    user.id === userId
+            );
+
+        users[userIndex].total_gold +=
+            rewardGold;
+
+        writeJson(userPath, users);
+    }
 
     writeJson(todoPath, todos);
 
