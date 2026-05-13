c398ea248d576be3fd53e2890a0b0d1a5b6a74ca
Hanjunhee-1 <hanjuni857@naver.com>
2026-05-13 23:16:09 +0900

    feat: DB 설계

---
+++ Server/Database/Importance.js
@@ -0,0 +1,38 @@
+const importance = {
+    LOWEST: {
+        text: "최하",
+        score: 1
+    },
+
+    LOW: {
+        text: "하",
+        score: 2
+    },
+
+    MID_LOW: {
+        text: "중하",
+        score: 3
+    },
+
+    MID: {
+        text: "중",
+        score: 4
+    },
+
+    MID_HIGH: {
+        text: "중상",
+        score: 5
+    },
+
+    HIGH: {
+        text: "상",
+        score: 6
+    },
+
+    HIGHEST: {
+        text: "최상",
+        score: 7
+    }
+}
+
+module.exports = importance;
